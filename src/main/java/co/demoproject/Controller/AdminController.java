2d21094d577b6b091006a7d07b3e9b33403e9b0d
bong <vythanhlam100@gmail.com>
2023-01-15 15:32:55 +0700

    package war file

--- src/main/java/co/demoproject/Controller/AdminController.java
+++ src/main/java/co/demoproject/Controller/AdminController.java
@@ -1,5 +1,10 @@
 package co.demoproject.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/admin")
 public class AdminController {
 
 }
